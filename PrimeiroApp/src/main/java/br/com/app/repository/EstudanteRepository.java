8d46ebe119bc80e77693fed79b5ff3b57026fdd4
João Carllos <jocasilvalima@gmail.com>
2023-10-21 13:12:33 -0300

    Criação dos Repositorios

---
+++ PrimeiroApp/src/main/java/br/com/app/repository/EstudanteRepository.java
@@ -0,0 +1,20 @@
+package br.com.app.repository;
+
+//importação da classe Turma
+import br.com.app.entity.Estudante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//anotação @Repository para o spring fazer a configuração automatica do projeto
+@Repository
+/*
+interface que extende JpaRepository para fazer a persistencia dos dados
+persistencia de dados é a forma de salvar os dados no banco de dados
+JpaRepository<Estudante, Integer> é o tipo de dado que vai ser salvo e o tipo da chave primaria
+Estudante é o tipo de dado que vai ser salvo
+Integer é o tipo da chave primaria
+*/
+public interface EstudanteRepository extends JpaRepository<Estudante, Integer> {
+
+
+}
---
+++ PrimeiroApp/src/main/java/br/com/app/repository/TurmaRepository.java
@@ -0,0 +1,19 @@
+package br.com.app.repository;
+
+//importação da classe Turma
+import br.com.app.entity.Turma;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+//anotação @Repository para o spring fazer a configuração automatica do projeto
+@Repository
+/*
+interface que extende JpaRepository para fazer a persistencia dos dados
+persistencia de dados é a forma de salvar os dados no banco de dados
+JpaRepository<Turma, String> é o tipo de dado que vai ser salvo e o tipo da chave primaria
+Turma é o tipo de dado que vai ser salvo
+String é o tipo da chave primaria
+*/
+public interface TurmaRepository extends JpaRepository<Turma, String> {
+
+}
