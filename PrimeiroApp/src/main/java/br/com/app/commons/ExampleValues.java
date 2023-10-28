ccb44fe3f3cd1c4d614a7b01fd674d5d79c9b1be
João Carllos <jocasilvalima@gmail.com>
2023-10-27 23:01:27 -0300

    Adicionado as classes ExampleValues no pacote commons.

---
+++ PrimeiroApp/src/main/java/br/com/app/commons/ExampleValues.java
@@ -0,0 +1,30 @@
+package br.com.app.commons;
+
+public class ExampleValues {
+    public static final String ID_ESTUDANTE = "1";
+    public static final String MATRICULA_PROFESSOR = "1";
+    public static final String PAGINA = "0";
+    public static final String ITENS_POR_PAGINA = "1";
+    public static final String ORDENACAO = "nome";
+public static final String TIPO_ORDERNACAO = "ASC";
+
+
+
+    public static final String exemploJsonEstudante = "{"
+            + "    \"nome\": \"João\",\r\n\""
+            + "    \"sobrenome\": \"Silva\",\r\n\""
+            + "    \"email\": \"joao@gmail.com\",\r\n\""
+            + "      \"data\": \"2021-09-01\"}";
+
+
+    public static final String exemploJsonProfessor = "{"
+            + "    \"nome\": \"Triellen Lima\",\r\n\""
+            + "    \"especializacao\": \"Programação\",\r\n\""
+            + "    \"telefone\": \"(11) 99999-9999\"}";
+
+
+    public static final String exemploJsonTurma = "{"
+            + "    \"nome\": \"2023-0001\",\r\n\""
+            + "    \"turno\": \"Manhã\",\r\n\""
+            + "    \"curso\": \"Sistemas de Informação\"}";
+}
